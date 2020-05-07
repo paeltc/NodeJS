5bb16bdefd63748e352f1391c210808296f82311
Paul Elvis Tcheumeni <hellotopaul@gmail.com>
2020-05-07 07:56:30 -0400

    href page redirection to carouselPage

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function(req,res){
   res.status(200).sendFile(__dirname + '/index.html');
 });
 
+app.get('/carouselPage', function(req,res){
+    res.status(200).sendFile(__dirname + '/carouselPage.html');
+  });
+
 var server = app.listen(process.env.PORT || '3000', function(){
   console.log('App listening on Port %s', server.address().port);
   console.log('Press Ctrl+C to quit');
