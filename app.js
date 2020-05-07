cdc8a6da178619eaa7cbf821efb41b9df9ada421
Paul Elvis Tcheumeni <hellotopaul@gmail.com>
2020-05-06 20:17:48 -0400

    NodeJS - Carousel

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.get('/', function(req,res){
+  res.status(200).sendFile(__dirname + '/index.html');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+var server = app.listen(process.env.PORT || '3000', function(){
+  console.log('App listening on Port %s', server.address().port);
+  console.log('Press Ctrl+C to quit');
 });
