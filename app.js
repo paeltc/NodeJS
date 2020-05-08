58bfaa8e0d19bc177173af8260ba22655ebc1061
Paul Elvis Tcheumeni <hellotopaul@gmail.com>
2020-05-07 23:51:33 -0400

    Forms 1

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
-  res.status(200).sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.status(200).sendFile(__dirname + '/index.html');
 });
 
-app.get('/carouselPage', function(req,res){
+app.get('/carouselPage', function (req, res) {
     res.status(200).sendFile(__dirname + '/carouselPage.html');
-  });
+});
 
-var server = app.listen(process.env.PORT || '3000', function(){
-  console.log('App listening on Port %s', server.address().port);
-  console.log('Press Ctrl+C to quit');
+app.get('/registerPage', function (req, res) {
+    res.status(200).sendFile(__dirname + '/registerPage.html');
+});
+
+var server = app.listen(process.env.PORT || '3000', function () {
+    console.log('App listening on Port %s', server.address().port);
+    console.log('Press Ctrl+C to quit');
 });
