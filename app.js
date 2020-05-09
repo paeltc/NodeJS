2c81b60d3e4ff7d0b6865b69670793e667f0f13a
Paul Elvis Tcheumeni <hellotopaul@gmail.com>
2020-05-08 23:29:31 -0400

    reload nodeJS

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var http = require('http')
+var path = require('path')
+var reload = require('reload')
+ 
+var app = express() 
+var publicDir = path.join(__dirname, 'views')
 
 app.get('/', function (req, res) {
-    res.status(200).sendFile(__dirname + '/index.html');
-});
+  res.sendFile(path.join(publicDir, '/index.html'))
+})
 
 app.get('/carouselPage', function (req, res) {
-    res.status(200).sendFile(__dirname + '/carouselPage.html');
-});
+    res.sendFile(path.join(publicDir, '/carouselPage.html'))
+})
 
 app.get('/registerPage', function (req, res) {
-    res.status(200).sendFile(__dirname + '/registerPage.html');
-});
-
-var server = app.listen(process.env.PORT || '3000', function () {
-    console.log('App listening on Port %s', server.address().port);
-    console.log('Press Ctrl+C to quit');
-});
+    res.sendFile(path.join(publicDir, '/registerPage.html'))
+})
+ 
+app.set('port', process.env.PORT || 3000)
+var server = http.createServer(app)
+ 
+reload(app).then(function (reloadReturned) {
+  server.listen(app.get('port'), function () {
+    console.log('Web server listening on port ' + app.get('port'))
+    console.log('Press Ctrl+C to quit')
+  })
+}).catch(function (err) {
+  console.error('Reload could not start, could not start server/sample app', err)
+})
